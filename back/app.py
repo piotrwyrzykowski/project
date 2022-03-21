cdd2c330b721f2da9095a844f505af9db28d0970
Piotr Wyrzykowski <piotrkwyrzykowski@gmail.com>
2022-03-21 21:59:19 +0000

    bmp redis

--- back/app.py
+++ back/app.py
@@ -2,44 +2,62 @@ from flask import Flask, request, jsonify
 from rejson import Client, Path
 import psycopg2
 import json
+import jsonschema
+from jsonschema import validate
 
 app = Flask(__name__)
 
 rj = Client(host='redis', port=6379, decode_responses=True)
 
-conn = psycopg2.connect(
-    host="db",
-    database="python_data",
-    user="postgres",
-    password="example")
-TABLE_NAME = "data"
+
+bmpSchema = {
+  "type": "object",
+  "properties": {
+    "hum": {
+      "type": "number"
+    },
+    "name": {
+      "type": "string"
+    },
+    "press": {
+      "type": "number"
+    },
+    "temp": {
+      "type": "number"
+    }
+  },
+  "required": [
+    "hum",
+    "name",
+    "press",
+    "temp"
+  ]
+}
+def validateJson(jsonData):
+    try:
+        validate(instance=jsonData, schema=bmpSchema)
+    except jsonschema.exceptions.ValidationError as err:
+        return False
+    return True
+
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST":
-        obj=request.get_json()
-        rj.jsonset('obj', Path.rootPath(), obj)
-        #cur = conn.cursor()
-        print(obj)
-
-        return "response"
+    if request.method == "POST":       
+        return "Wrong api call"
     else:
-        response = rj.jsonget('obj',Path('.sensor'))
-        #response = rj.jsonget('obj')
-        #cur = conn.cursor()
-        #
-        # cur.execute("select * from %s where name = 'BMP'"),TABLE_NAME
-        #foo = json.dumps(cur.fetchall())
-        #cur.close()
-        #print(foo)
-        return response
+        return "Wrong api call"
+
 @app.route("/redis/bmp", methods=["POST", "GET"])
 def redis_bmp():
     if request.method == "POST":
         obj=request.get_json()
-        rj.jsonset('bmp', Path.rootPath(), obj)
-        print(obj)
-        return "bmp to redis"
+        isValid = validateJson(obj)
+        if isValid:
+            rj.jsonset('bmp', Path.rootPath(), obj)
+            return "valid json data"
+        else:
+            return "json is invalid"       
     else:
         response = rj.jsonget('bmp',Path.rootPath())
         return response
--- front/app.py
+++ front/app.py
@@ -5,19 +5,14 @@ import sys
 app = Flask(__name__)
 
 backend_URL = "http://backend:5001"
-log = []
-
 
 def get_redis_bmp():
-
     return requests.get(url=backend_URL+"/redis/bmp").json()
 
 
 @app.route("/", methods=["POST","GET"])
 def index():
-    
     return render_template("index.html", data_bmp=get_redis_bmp())
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
