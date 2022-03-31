018ef0aafcdc3683cb784e8e22cb7ee545c0c761
Piotr Wyrzykowski <piotrkwyrzykowski@gmail.com>
2022-03-31 16:28:55 +0000

    modifying jsonschema

--- back/app.py
+++ back/app.py
@@ -16,44 +16,38 @@ rj = Client(host='redis', port=6379, decode_responses=True)
 bmpSchema = {
   "type": "object",
   "properties": {
-    "hum": {
-      "type": "number"
-    },
-    "name": {
-      "type": "string"
-    },
-    "press": {
-      "type": "number"
-    },
-    "temp": {
-      "type": "number"
-    }
+    "name": { "type": "string" },
+    "temp": { "type": "number" },
+    "hum": { "type": "number" },
+    "press": { "type": "number" },
+    "pm1.0": { "type": "number" },
+    "pm2.5": { "type": "number" },
+    "pm10": { "type": "number" }
   },
-  "required": [
-    "hum",
-    "name",
-    "press",
-    "temp"
-  ]
+  "if": {
+    "properties": {
+      "name": { "const": "BMP" }
+    },
+    "required": ["name"]
+  },
+  "then": { "required": ["temp","hum","press"] }
 }
 def validateJson(jsonData):
     try:
-        validate(instance=jsonData, schema=bmpSchema)
+        validate(instance=jsonData, schema=(bmpSchema))
     except jsonschema.exceptions.ValidationError as err:
         return False
     return True
 
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
 
 def addTimeStamp(jsonData):
       epoch = datetime.datetime.now().timestamp()
-      #epoch = epoch.replace(tzinfo=timezone.utc)
       jsonData["timestamp"] = round(epoch)
       return jsonData
+
+def sendToRedis(jsonData):
+  data = jsonData['name']
+  rj.jsonset( data, Path.rootPath(), addTimeStamp(jsonData))
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -68,12 +62,13 @@ def redis_bmp():
         obj=request.get_json()
         isValid = validateJson(obj)
         if isValid:
-            rj.jsonset('bmp', Path.rootPath(), addTimeStamp(obj))
+            sendToRedis(obj)
+            #rj.jsonset('bmp', Path.rootPath(), addTimeStamp(obj))
             return "valid json data"
         else:
             return "json is invalid"       
     else:
-        response = rj.jsonget('bmp',Path.rootPath())
+        response = rj.jsonget('BMP',Path.rootPath())
         return response
 
 if __name__ == "__main__":
