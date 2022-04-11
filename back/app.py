0ac66b3682fec538d1242c1b5d3e89cabdf69f10
Piotr Wyrzykowski <piotrkwyrzykowski@gmail.com>
2022-04-11 20:09:48 +0000

    graph

--- back/app.py
+++ back/app.py
@@ -8,6 +8,11 @@ import datetime
 from datetime import date, timezone
 import time
 
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
+
 app = Flask(__name__)
 
 rj = Client(host='redis', port=6379, decode_responses=True)
@@ -16,7 +21,7 @@ rj = Client(host='redis', port=6379, decode_responses=True)
 bmpSchema = {
   "type": "object",
   "properties": {
-    "name": { "type": "string" },
+    "name": { "enum": ["BMP", "PMS"] },
     "temp": { "type": "number" },
     "hum": { "type": "number" },
     "press": { "type": "number" },
@@ -40,18 +45,18 @@ bmpSchema = {
         "required": ["name"]
       },
       "then": {
-        "required": ["pm1.0","pm2.5","pm10"]
+        "required": ["pm1_0","pm2_5","pm10"]
         }
     }
   ]
 }
+
 def validateJson(jsonData):
     try:
         validate(instance=jsonData, schema=(bmpSchema))
     except jsonschema.exceptions.ValidationError as err:
         return False
     return True
-
 
 def addTimeStamp(jsonData):
       epoch = datetime.datetime.now().timestamp()
@@ -61,6 +66,15 @@ def addTimeStamp(jsonData):
 def sendToRedis(jsonData):
   data = jsonData['name']
   rj.jsonset( data, Path.rootPath(), addTimeStamp(jsonData))
+
+def sendToInfluxdb(jsonData):
+  with InfluxDBClient(url="http://influxdb:8086", token="mytoken", org="pwr", debug=True) as client:
+    with client.write_api(write_options=SYNCHRONOUS) as write_api:
+        loaded = json.loads( '{"measurement": "BMP", "fields":{"temp": 234, "hum": 20, "press": 1021}}')
+        #loaded = json.loads( '{"measurement": "Messungen", "tags": {"Ort": "Rxxxx"}, "fields": {"temp1": 2.6, "hum1": 66.0, "temp2": 2.3, "hum2": 81.0, "temp3": 22.0, "hum3": 32.0}}')
+
+        write_api.write(bucket="bar", record=loaded)
+
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -76,12 +90,27 @@ def redis_bmp():
         isValid = validateJson(obj)
         if isValid:
             sendToRedis(obj)
-            #rj.jsonset('bmp', Path.rootPath(), addTimeStamp(obj))
+            sendToInfluxdb(obj)
             return "valid json data"
         else:
             return "json is invalid"       
     else:
         response = rj.jsonget('BMP',Path.rootPath())
+        return response
+
+@app.route("/redis/pms", methods=["POST", "GET"])
+def redis_pms():
+    if request.method == "POST":
+        obj=request.get_json()
+        isValid = validateJson(obj)
+        if isValid:
+            sendToRedis(obj)
+            sendToInfluxdb(obj)
+            return "valid json data"
+        else:
+            return "json is invalid"       
+    else:
+        response = rj.jsonget('PMS',Path.rootPath())
         return response
 
 if __name__ == "__main__":
--- front/app.py
+++ front/app.py
@@ -7,14 +7,23 @@ import sys
 app = Flask(__name__)
 
 backend_URL = "http://backend:5001"
-
+ip = "pms"
 def get_redis_bmp():
     return requests.get(url=backend_URL+"/redis/bmp").json()
+
+def get_redis_pms():
+    return requests.get(url=backend_URL+"/redis/pms").json()
 
 
 @app.route("/", methods=["POST","GET"])
 def index():
-    return render_template("index.html", data_bmp=get_redis_bmp())
+    ulll='http://192.168.1.240:3000/d-solo/oT0yskU7z/new-dashboard?orgId=1&panelId=2"'
+    return render_template("index.html", data_bmp=get_redis_bmp(), data_pms=get_redis_pms(), ip=ip)
+
+@app.route("/pms", methods=["POST","GET"])
+def pms():
+    ulll='http://192.168.1.240:3000/d-solo/oT0yskU7z/new-dashboard?orgId=1&panelId=2"'
+    return render_template("graphs.html", data_bmp=get_redis_bmp(), data_pms=get_redis_pms(), url=ulll)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
