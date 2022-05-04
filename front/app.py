6e7ddaa5c5948e78e23df4862535da4571153b1f
Piotr Wyrzykowski <piotrkwyrzykowski@gmail.com>
2022-05-04 14:44:47 +0000

    front update

--- front/app.py
+++ front/app.py
@@ -13,24 +13,25 @@ app = Flask(__name__)
 backend_URL = "http://backend:5001"
 ip = "graph"
 def get_redis_bmp():
-    return requests.get(url=backend_URL+"/api/bmp").json()
+    try:
+      return requests.get(url=backend_URL+"/api/bmp").json()
+    except:
+      return {"fields":{"hum":"N/A","press":"N/A","temp":"N/A"},"measurement":"BMP","timestamp":"N/A"}
 
 def get_redis_pms():
-    return requests.get(url=backend_URL+"/api/pms").json()
-
+    try:
+      return requests.get(url=backend_URL+"/api/pms").json()
+    except:
+      return {"fields":{"pm10_0":"N/A","pm1_0":"N/A","pm2_5":"N/A"},"measurement":"PMS","timestamp":"N/A"}
 
 @app.route("/", methods=["POST","GET"])
 def index():
     return render_template("index.html", data_bmp=get_redis_bmp(), data_pms=get_redis_pms(), ip=ip)
 
 @app.route("/graph", methods=["POST","GET"])
-def pms():
-    tz = pytz.timezone("Europe/Warsaw")
-    epoch = round(datetime.datetime.now(tz).timestamp()) * 1000
-    ulll ='http://stacyjka.ddns.net:3000/d-solo/oT0yskU7z/new-dashboard?orgId=1&from='
-    ulll = ulll + str(epoch - 3600 ) + '&to=' + str(epoch) + '&panelId=2'
+def graph():
     ulll = 'http://stacyjka.ddns.net:3000/d/oT0yskU7z/new-dashboard?orgId=1&viewPanel=2'
-    return render_template("graphs.html", data_bmp=get_redis_bmp(), data_pms=get_redis_pms(), url=ulll)
+    return render_template("graphs.html", url=ulll)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
